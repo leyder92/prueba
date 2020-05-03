5a03c76c024c8b3b74d2cf8b37e5ad84d8a8e94d
Leyder <leyderperezglv@gmail.com>
2020-05-02 19:45:11 -0600

    Commit base de prueba

---
+++ src/app/services/data-db.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { MessageI } from '../models/message.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DataDbService {
+  private contactCollection: AngularFirestoreCollection<MessageI>;
+  constructor(private afs: AngularFirestore) {
+    this.contactCollection = afs.collection<MessageI>('contacs');
+   }
+
+   saveMessage (newContact: MessageI): void{
+     this.contactCollection.add(newContact);
+   }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+   firebaseConfig : {
+    apiKey: "AIzaSyCsmLqXYmOzcRUUA-lcBBwPahc5h5g3ySo",
+    authDomain: "aplicacion-angular-dc2b4.firebaseapp.com",
+    databaseURL: "https://aplicacion-angular-dc2b4.firebaseio.com",
+    projectId: "aplicacion-angular-dc2b4",
+    storageBucket: "aplicacion-angular-dc2b4.appspot.com",
+    messagingSenderId: "889437395190",
+    appId: "1:889437395190:web:09fe97cd9bbd944d86f4d7",
+    measurementId: "G-KTCXVSJXZP"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
